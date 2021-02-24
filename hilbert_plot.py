2a56cd4d34dff548a2b94ecda1c6bdc9822b2259
Sentienta Sins <sentientasins@gmail.com>
2021-02-25 01:34:16 +0530

    created hilbert plot

---
+++ hilbert_plot.py
@@ -0,0 +1,27 @@
+from matplotlib.pyplot import subplot, plot, show, title, suptitle, figure, legend
+from scipy.signal import hilbert
+from pandas import read_csv
+from sys import argv
+ 
+def plotDetection(File):
+	df = read_csv(File)
+	#dic = df.to_string()
+
+	for detector in df:
+		if detector[-2] == "E":
+			figure(0, figsize=(15, 15))
+		elif detector[-2] == "H":
+			figure(1, figsize=(15, 15))
+		for dimension in range(len(df[detector][0][2:-2].split())):
+			subplot(2, 2, dimension + 1)
+			plot(abs(hilbert([float(x[2:-2].split()[dimension]) for x in df[detector]])), label=detector)
+			#plot([float(x[2:-2].split()[dimension]) for x in df[detector]])
+			title(detector[-2] + "(" + ["x", "y", "z"][dimension] + ")")
+		#suptitle(detector)
+		#show()
+	legend()
+	show()
+
+
+if __name__ == "__main__":
+	plotDetection(argv[1])
