c4b6529c1e895a5ed06335a463315550858d1875
Sentienta Sins <sentientasins@gmail.com>
2021-02-26 22:15:27 +0530

    indentation corrections after unnecessacry VSC interference (never again)

--- plot_fdtd_detection.py
+++ plot_fdtd_detection.py
@@ -10,33 +10,33 @@ simulation name (optional): Saves the simulation folder with this name along wit
 
 
 def plotDetection(File, timestep):
-    df = read_csv(File)
-    #dic = df.to_string()
-    if int(timestep) == -1:
-        timestep = len(df) - 1
+	df = read_csv(File)
+	#dic = df.to_string()
+	if int(timestep) == -1:
+		timestep = len(df) - 1
 
-    for detector in df:
-        figure(figsize=(15, 15))
-        for dimension in range(len(df[detector][0][1:-1].split("\n ")[0][1:-1].split())):
-            subplot(2, 2, dimension + 1)
-            plot([float(x[1:-1].split()[dimension]) for x in df[detector][int(timestep)][1:-1].split("\n ")])
-            title(detector[-2] + "(" + ["x", "y", "z"][dimension] + ")")
-        suptitle(detector)
-        show()
+	for detector in df:
+		figure(figsize=(15, 15))
+		for dimension in range(len(df[detector][0][1:-1].split("\n ")[0][1:-1].split())):
+			subplot(2, 2, dimension + 1)
+			plot([float(x[1:-1].split()[dimension]) for x in df[detector][int(timestep)][1:-1].split("\n ")])
+			title(detector[-2] + "(" + ["x", "y", "z"][dimension] + ")")
+		suptitle(detector)
+		show()
 
-    #for dimension in range(len(df["detector (E)"][0][1:-1].split())):
-    #subplot(2, len(df["detector (E)"][0][1:-1].split()), dimension + 1)
-    #plot([x[1:-1].split()[dimension] for x in df["detector (E)"]])
-    #title("E(" + ["x", "y", "z"][dimension] + ")")
+	#for dimension in range(len(df["detector (E)"][0][1:-1].split())):
+	#subplot(2, len(df["detector (E)"][0][1:-1].split()), dimension + 1)
+	#plot([x[1:-1].split()[dimension] for x in df["detector (E)"]])
+	#title("E(" + ["x", "y", "z"][dimension] + ")")
 
-    #subplot(2, len(df["detector (H)"][0][1:-1].split()), dimension + 4)
-    #plot([x[1:-1].split()[dimension] for x in df["detector (H)"]])
-    #title("H(" + ["x", "y", "z"][dimension] + ")")
+	#subplot(2, len(df["detector (H)"][0][1:-1].split()), dimension + 4)
+	#plot([x[1:-1].split()[dimension] for x in df["detector (H)"]])
+	#title("H(" + ["x", "y", "z"][dimension] + ")")
 
-    #show()
+	#show()
 
 
 if __name__ == "__main__":
-    if len(argv) < 3:
-        argv.append(-1)
-    plotDetection(argv[1], argv[2])
+	if len(argv) < 3:
+		argv.append(-1)
+	plotDetection(argv[1], argv[2])
