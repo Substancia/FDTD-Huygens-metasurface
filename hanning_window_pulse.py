f6de5bee7a0e0822b56baa6b4d5fff34a04dd571
Sentienta Sins <sentientasins@gmail.com>
2021-03-17 16:58:01 +0530

    added integral plot

--- hanning_window_pulse.py
+++ hanning_window_pulse.py
@@ -11,10 +11,11 @@ dt = 4e-15
 f = 3e8 / 1550e-9
 timesteps = 360
 t1 = int(2 * pi / (f * dt / 5))
-src = zeros(timesteps)
+src, integral = zeros(timesteps), zeros(timesteps)
 for i in range(0, t1):
 	src[i] = Hanning(f, i * dt, 5)
-subplot(1, 2, 1)
+	integral[i] = integral[i-1] + src[i]
+subplot(2, 2, 1)
 plot(src)
 plot(hilbert(src).imag)
 plot(abs(hilbert(src)))
@@ -24,7 +25,7 @@ ylabel("Amplitude")
 xlim(0, t1)
 #show()
 
-subplot(1, 2, 2)
+subplot(2, 2, 2)
 spec = fft.fft(src)
 freq = fft.fftfreq(spec.size, d=dt)
 freq = 2 * pi * freq
@@ -33,6 +34,14 @@ title("Frequency response of Hann window")
 xlabel("Frequency")
 ylabel("Manitude")
 xlim(0, 2 * f)
+
+subplot(2, 2, 3)
+plot(src)
+plot(integral)
+title("Integral plot")
+xlabel("Time steps")
+ylabel("Amplitude")
+xlim(0, t1)
 
 suptitle("5 cycle Hann window pulse")
 show()
