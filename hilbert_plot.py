7aba5656695cfad1bc735e2b292da77299b4efcc
Sentienta Sins <sentientasins@gmail.com>
2021-03-31 23:05:23 +0530

    defaulted first detector element from each detector for analysis

--- hilbert_plot.py
+++ hilbert_plot.py
@@ -1,4 +1,4 @@
-from numpy import array, where, arctan, degrees
+from numpy import array, where, arcsin, degrees
 from matplotlib.pyplot import subplot, plot, show, title, suptitle, figure, legend, xlabel, ylabel
 from scipy.signal import hilbert
 from pandas import read_csv
@@ -12,7 +12,8 @@ Arguments:-
 filename (string)
 """
  
-def plotDetection(File, detectorElement, specificPlot, angleParams):
+def plotDetection(File, specificPlot, angleParams):
+	detectorElement = 0
 	df = read_csv(File)
 	#dic = df.to_string()
 	maxArray = {}
@@ -32,7 +33,7 @@ def plotDetection(File, detectorElement, specificPlot, angleParams):
 			# if specificPlot, plot on 1x1, else plot on 2x2
 			subplot(2 - int(specificPlot is not None), 2 - int(specificPlot is not None), dimension + 1 if specificPlot is None else 1)
 			#plot(abs(hilbert([float(x[2:-2].split()[dimension]) for x in df[detector]])), label=detector)
-			hilbertPlot = abs(hilbert([float(x[1:-1].split("\n ")[0][1:-1].split()[dimension]) for x in df[detector]]))
+			hilbertPlot = abs(hilbert([float(x[1:-1].split("\n ")[detectorElement][1:-1].split()[dimension]) for x in df[detector]]))
 			plot(hilbertPlot, label=detector)
 			title(detector[-2] + "(" + ["x", "y", "z"][dimension] + ")")
 			if detector[-2] not in maxArray:
@@ -73,19 +74,19 @@ def plotDetection(File, detectorElement, specificPlot, angleParams):
 	if angleParams is not None:
 		specificPlot = specificPlot[0] + str(["x", "y", "z"].index(specificPlot[1]))
 		print(maxArray[specificPlot[0]][specificPlot[1]])
-		angle = degrees(arctan((maxArray[specificPlot[0]][specificPlot[1]][angleParams[1]][1] - maxArray[specificPlot[0]][specificPlot[1]][angleParams[0]][1]) / (((angleParams[1] + len(maxArray[specificPlot[0]][specificPlot[1]])) % len(maxArray[specificPlot[0]][specificPlot[1]]) - angleParams[0]) * angleParams[2])))
+		angleParams[1] = (angleParams[1] + len(maxArray[specificPlot[0]][specificPlot[1]])) % len(maxArray[specificPlot[0]][specificPlot[1]])
+		angleParams[0] = (angleParams[0] + len(maxArray[specificPlot[0]][specificPlot[1]])) % len(maxArray[specificPlot[0]][specificPlot[1]])
+		angle = degrees(arcsin(0.7*(maxArray[specificPlot[0]][specificPlot[1]][angleParams[1]][1] - maxArray[specificPlot[0]][specificPlot[1]][angleParams[0]][1]) / ((angleParams[1] - angleParams[0]) * angleParams[2])))
 		print("Angle (based on " + specificPlot[0]+["x", "y", "z"][int(specificPlot[1])] + "):", angle)
 		return angle
 
 
 if __name__ == "__main__":
 	if len(argv) < 3:
-		argv.append("0")
-	if len(argv) < 4:
 		argv.append(None)
-	if len(argv) > 4:
-		argv[4] = [int(x) for x in argv[4][1:-1].split(",")]
-	if len(argv) < 5:
+	if len(argv) > 3:
+		argv[3] = [int(x) for x in argv[3][1:-1].split(",")]
+	if len(argv) < 4:
 		#argv.append([0, 2, 2])
 		argv.append(None)
-	plotDetection(argv[1], argv[2], argv[3], argv[4])
+	plotDetection(argv[1], argv[2], argv[3])
